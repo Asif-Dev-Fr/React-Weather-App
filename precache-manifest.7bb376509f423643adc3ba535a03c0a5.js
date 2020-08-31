fdd6c02ce33efd586b3b6f8f34cb11b7dff69d47
Kassam Ali Asif <asif.k.dev@gmail.com>
2020-08-31 18:17:15 +0200

    Updates

---
+++ precache-manifest.7bb376509f423643adc3ba535a03c0a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b830b50c3e4fc6d4d23a81d9a3c0c96",
+    "url": "/React-Weather-App/index.html"
+  },
+  {
+    "revision": "dd0efc6bf083d15d1bf6",
+    "url": "/React-Weather-App/static/css/main.c3c8c8e3.chunk.css"
+  },
+  {
+    "revision": "72fe3d1038947c2660c3",
+    "url": "/React-Weather-App/static/js/2.97ab481c.chunk.js"
+  },
+  {
+    "revision": "da75538c52411804769b514107a743de",
+    "url": "/React-Weather-App/static/js/2.97ab481c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd0efc6bf083d15d1bf6",
+    "url": "/React-Weather-App/static/js/main.33f51d4a.chunk.js"
+  },
+  {
+    "revision": "6e57c8822c235b582909",
+    "url": "/React-Weather-App/static/js/runtime-main.bcf8141b.js"
+  }
+]);
